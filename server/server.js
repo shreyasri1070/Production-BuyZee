3cb4ce82c9cc86b087f923f67f177024568a28c4
shreyasri1070 <shreyasri1070@gamil.com>
2024-09-18 00:55:04 +0530

    production

--- server/server.js
+++ server/server.js
@@ -7,7 +7,12 @@ import ConnectDB from '../config/db.js'; // In ES6 we have to give module extens
 import authRoute from '../Routes/authroute.js';
 import categoryRoutes from '../Routes/categoryRoutes.js';
 import productRoutes from '../Routes/productRoutes.js';
-import cors from 'cors'
+import cors from 'cors';
+import path from 'path' ;
+
+import { fileURLToPath } from 'url';
+const __filename=fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__filename);
 const app=express();
 //config env
 dotenv.config({path:'../.env'});
@@ -20,16 +25,16 @@ ConnectDB();
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'))
+
+app.use(express.static(path.join(__dirname,'../client/build')))
 //routes
 app.use("/api/v1/auth",authRoute)
 app.use("/api/v1/category",categoryRoutes)
 app.use("/api/v1/product",productRoutes)
 
 //api
-app.get('/',(req,res)=>{
-    res.send({
-        message:'Welcome to E-Commerce app'
-    })
+app.use('*',function(req,res){
+    res.sendFile(path.join(__dirname,'../client/build/index.html'))
 })
 //Port
 const PORT=process.env.PORT||8000;
